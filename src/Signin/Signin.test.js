9eab8207fb9937a680bdd2bf9b1f4648f0d02766
Daniela Carey <danielafcarey@gmail.com>
2018-05-26 17:45:51 -0600

    Implement authoize via Spotify
    To do: get access token after redirect

--- src/Signin/Signin.test.js
+++ src/Signin/Signin.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import Signin from './Signin';
+import * as authorize from '../spotify/authorizeSpotify';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,48 +13,21 @@ it('renders without crashing', () => {
 describe('Signin', () => {
 
   it('matches the snapshot', () => {
+    const wrapper = shallow(<Signin />);
 
+    expect(wrapper).toMatchSnapshot();
   })
 
-  it('has a default state of email and password', () => {
+  describe('handleClick', () => {
 
+    it('calls authorizeSpotify', () => {
+      const wrapper = shallow(<Signin />);
+      authorize.authorizeSpotify = jest.fn();
+
+      wrapper.find('button').simulate('click');
+
+      expect(authorize.authorizeSpotify).toHaveBeenCalled();
+    })
   })
-
-  describe('handleChange', () => {
-
-    it('updates the state on change in the email input', () => {
-
-    })
-
-    it('updates the state on change in the password input', () => {
-
-    })
-
-  })
-
-  describe('handleSubmit', () => {
-    
-    it('makes an api call to get the userId', () => {
-
-    })
-
-    it('sends the userId to state via updateCurrentUser', () => {
-
-    })
-
-    it('shows an error message if signin was unsuccessful', () => {
-
-    })
-
-  })
-
-  describe('mapDispatchToProps', () => {
-
-    it('calls dispatch with the correct arguments', () => {
-
-    })
-
-  })
-
 
 })
