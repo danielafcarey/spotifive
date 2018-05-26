557f36af78f581da3b843847a31b442ac30b6bdb
Daniela Carey <danielafcarey@gmail.com>
2018-05-26 17:45:51 -0600

    Implement authoize via Spotify
    To do: get access token after redirect

--- src/Signin/Signin.js
+++ src/Signin/Signin.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { authorizeSpotify } from '../spotify/authorizeSpotify';
 
 const Signin = (props) => {
 
   return (
     <div className='signin'>
+      <p>
+        Spotifive helps you discover new music by generating a playlist of new music to try based on the artists you choose.
+      </p>
+      <p>To begin, sign in with Spotify:</p>
+      <button
+        onClick={ handleClick } 
+      >Sign in with Spotify</button>
     </div>
   )
+}
+
+const handleClick = (event) => {
+  authorizeSpotify();
 }
 
 export default Signin;
