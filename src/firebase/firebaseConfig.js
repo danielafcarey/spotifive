596f7b3207d0503d1f0ec4ecfef585503db80a56
Daniela Carey <danielafcarey@gmail.com>
2018-05-26 15:26:51 -0600

    Connect firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBJpJGqc3bF-GP0n8HN9sq0Eh7UFxY7jEM",
+    authDomain: "try-spotifive.firebaseapp.com",
+    databaseURL: "https://try-spotifive.firebaseio.com",
+    projectId: "try-spotifive",
+    storageBucket: "try-spotifive.appspot.com",
+    messagingSenderId: "999236788446"
+  };
+
+export {
+  config
+};
---
+++ src/spotify/index.js
@@ -0,0 +1,2 @@
+const spotifyClientId = '035fb661a97a4ffb938eebc01c0dd4d3';
+const spotifyClientSecret = 'bc9e18ee528d4dfa8b9417a6cf5fecf4';
