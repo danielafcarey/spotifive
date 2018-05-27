5020393b805526ec049325673833c88773bb8106
Daniela Carey <danielafcarey@gmail.com>
2018-05-26 19:42:01 -0600

    Add updateAccessToken action, reducer, and tests and to Authorize component

--- src/Authorize/Authorize.test.js
+++ src/Authorize/Authorize.test.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
-import Authorize from './Authorize';
+import * as cleaner from '../cleaner';
+import {
+  Authorize,
+  mapDispatchToProps
+} from './Authorize';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Authorize />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import {
+  updateAccessToken
+} from '../actions';
+cleaner.getAccessToken = jest.fn().mockImplementation(() => 'cleanedAccessToken')
+
+
 
 describe('Authorize', () => {
+  let wrapper;
+  let mockProps;
+
+  beforeEach(() => {
+    mockProps = {
+      updateAccessToken: jest.fn(),
+    }
+    window.location = { hash: 'string' }
+    wrapper = shallow(<Authorize { ...mockProps }/>);
+  }) 
 
   it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 
+  describe('componentDidMount', () => {
+
+    it('calls updateAccessToken with the correct argumenst', () => {
+      wrapper = shallow(<Authorize { ...mockProps }/>);
+      const expected = cleaner.getAccessToken();
+
+      expect(wrapper.instance().props.updateAccessToken).toHaveBeenCalledWith(expected);
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+
+    it('should call dispatch with the correct arguments', () => {
+      const dispatch = jest.fn();
+      const mockAccessToken = 'token';
+      const mockAction = {
+        type: 'UPDATE_ACCESS_TOKEN',
+        accessToken: mockAccessToken
+      }
+      const mappedProps = mapDispatchToProps(dispatch);
+
+      mappedProps.updateAccessToken(mockAction.accessToken);
+
+      expect(dispatch).toHaveBeenCalledWith(mockAction);
+    })
   })
 
 })
