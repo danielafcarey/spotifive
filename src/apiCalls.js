bc565050e81b767dd6f13e67f0622a1cc6c15048
Daniela Carey <danielafcarey@gmail.com>
2018-05-27 11:51:41 -0600

    Add api calls to collect and combine user playlists

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -18,7 +18,46 @@ const getUserData = async (accessToken) => {
   }
 }
 
+const getUserPlaylists = async (accessToken) => {
+  const url = 'https://api.spotify.com/v1/me/playlists?limit=50';
+  const optionsObject = {
+    headers: { 'Authorization': `Bearer ${accessToken}` }
+  };
+  try {
+    const response = await fetch(url, optionsObject);
+    if (response.status === 200) {
+      const data = await response.json();
+      return await getAllPlaylists(data.items, data.next, optionsObject);
+    } else {
+      throw Error(response.status)
+    }
+  } catch (error) {
+    throw error
+  }
+}
+
+const getAllPlaylists = async (prevPlaylists, nextPage, optionsObject) => {
+  if (nextPage === null) {
+    return prevPlaylists;
+  }
+  
+  try {
+    const response = await fetch(nextPage, optionsObject);
+    if (response.status === 200) {
+      const data = await response.json();
+      const combinedPlaylists = [...data.items, ...prevPlaylists] 
+      getAllPlaylists(combinedPlaylists, data.next, optionsObject)
+    } else {
+      throw Error(response.status)
+    }
+  } catch (error) {
+    throw error
+  }
+
+}
 
 export {
-  getUserData
+  getUserData,
+  getUserPlaylists,
+  getAllPlaylists
 }
