ff87f00fcd46e83ef5550f32393c3cea11fd517c
Daniela Carey <danielafcarey@gmail.com>
2018-05-28 20:33:21 -0600

    Stub mstp and mdtp for Search

--- src/Search/Search.test.js
+++ src/Search/Search.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
-import Search from './Search';
+import {
+  Search,
+  mapStateToProps,
+  mapDispatchToProps
+} from './Search';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -46,6 +50,13 @@ describe('Search', () => {
   })
 
   describe('handleSubmit', () => {
+    let wrapper;
+    let mockEvent;
+
+    beforeEach(() => {
+      wrapper = shallow(<Search />)
+      mockEvent = { preventDefault: () => {} }
+    })
 
     it('gets the artist info and top song ids', () => {
       // 2 fetch calls - one to get artist id and one to get top 5 songs
@@ -53,6 +64,12 @@ describe('Search', () => {
 
     it('updates the store with new artist info', () => {
 
+    })
+
+    it('resets the state to an empty string', () => {
+      wrapper.instance().handleSubmit;
+
+      expect(wrapper.state('searchInput')).toEqual('');
     })
 
   })
