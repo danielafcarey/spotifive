ff4ad1423f750935afdb6b694a9e4e33256d02e8
Daniela Carey <danielafcarey@gmail.com>
2018-05-30 17:22:10 -0600

    Write and pass ArtistCard test

--- src/ArtistCard/ArtistCard.js
+++ src/ArtistCard/ArtistCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ArtistCard = ({ name, id, selectArtist }) => {
   return (
     <div 
+      className='artist-card'
       onClick={ () => selectArtist(id) }
     >
       <p>{ name }</p>    
--- src/ArtistCard/ArtistCard.test.js
+++ src/ArtistCard/ArtistCard.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ArtistCard from './ArtistCard';
 
 describe('ArtistCard', () => {
 
   it('should match the snapshot', () => {
     const mockProps = {
-      name: 'Daniela'
+      name: 'Daniela',
+      id: 'abc',
+      selectArtist: jest.fn()
     }
     const wrapper = shallow(<ArtistCard { ...mockProps }/>);
 
