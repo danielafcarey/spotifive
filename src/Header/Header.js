ea9b18632f2351a784fc2e4ec781b4f521bed2a1
Daniela Carey <danielafcarey@gmail.com>
2018-06-03 14:14:02 -0600

    Show user image in header if signed in

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Header = () => {
+const Header = (props) => {
   const logo = require('../images/logo.png');
 
   return (
     <div className='Header' >
+      <img 
+        className='profile-photo'
+        src={ props.image ? props.image : '' } 
+        alt='' />
       <h1>Spotifive</h1> 
-      <img src={ logo } alt='' />
+      <img 
+        className='logo'
+        src={ logo } 
+        alt='logo' />
     </div>
   )
-}
+};
 
-export default Header;
+const mapStateToProps = (state) => ({
+  image: state.user.userInfo.image
+});
+
+export default connect(mapStateToProps)(Header);
